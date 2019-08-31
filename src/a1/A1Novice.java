94125dad00bb6de89330134cbc19cf0d62819273
salma <salma@salma-pc>
2019-08-31 11:25:17 -0400

    Novice runs and produces the proper output

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,34 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		
+		int customers = scan.nextInt();
+		
+		for (int i = customers; i >= 0; i--) {
+			char first_name = scan.next().charAt(0);
+			
+			String last_name = scan.next();
+			
+			int total = scan.nextInt();
+			
+			double temp = 0;
+			
+			for (int y = 0; y < total; y ++) {
+				int amount = scan.nextInt();
+				
+				String item = scan.next();
+				
+				double price = scan.nextDouble();
+				
+				temp += (amount * price); 
+			}
+			String result = String.format("%.2f", temp);
+			
+			System.out.println(first_name + ". " + last_name + ": " + result);
+			
+		}
+		
+		scan.close();
 		
 	}
 }

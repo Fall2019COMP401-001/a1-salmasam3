0b7413be68e82bcabdfa2fc958d23feb4361014d
salma <salma@salma-pc>
2019-09-01 23:35:01 -0400

    Jedi Runs

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -22,23 +22,27 @@ public class A1Jedi {
 		int[] counter = new int[itemTotal];
 		int[] numbers = new int[itemTotal];
 		
+		
 		for (int n = 0; n < customers; n++) {
 			String name = scan.next() + scan.next();
 			int numberOfItems = scan.nextInt();
+			String[] foods = new String[numberOfItems];
 			
 			for (int y = 0; y < numberOfItems; y++) {
 				int amount = scan.nextInt();
-				String item = scan.next();
+				foods[y] = scan.next();
 				
 				for (int i = 0; i < items.length; i++) {
-					if(item.equals(items[i])) {
+					
+					if(foods[y].equals(items[i])) {
 						counter[i] += amount;
 						if(numbers[i] < 2) {
 							numbers[i] ++;
 						}
-					}
+					} 
 				}
 			}
+			
 		}	
 		
 		
